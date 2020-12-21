c0ba7b14aeb1544ce666eb389718f6ee4998c8dd
Sergey <sergei.kuharyonok@yandex.by>
2020-12-21 21:42:14 +0300

    4 test refactoring,add storybook router decorator

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import App from './App'
-import {ReduxStoreProviderDecorator} from '../stories/decorators/ReduxStoreProviderDecorator'
+import {BrowserRouterDecorator, ReduxStoreProviderDecorator} from '../stories/decorators/ReduxStoreProviderDecorator'
 
 export default {
     title: 'App Stories',
     component: App,
-    decorators: [ReduxStoreProviderDecorator]
+    decorators: [ReduxStoreProviderDecorator,BrowserRouterDecorator]
 }
 
 export const AppBaseExample = (props: any) => {
