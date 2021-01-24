9642ffa4667a7bb438269059250b8e2310b900cd
Sergey-lang <sergei.kuharyonok@yandex.by>
2021-01-24 23:20:41 +0300

    Updates

---
+++ precache-manifest.c337305bbf2f82b540aec0e5cdeeeea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24e1496be9639c599400bc517fd39ec",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "5229ffd12f7303cc552a",
+    "url": "/Todo-list/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "f8c38a363f3e58a4dfb5",
+    "url": "/Todo-list/static/js/2.caea7437.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todo-list/static/js/2.caea7437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5229ffd12f7303cc552a",
+    "url": "/Todo-list/static/js/main.a1a57ed8.chunk.js"
+  },
+  {
+    "revision": "3047b565385991166f3a",
+    "url": "/Todo-list/static/js/runtime-main.b9f5889f.js"
+  }
+]);
